57418642615ec24ac40ded74c52d4fd4bf104ea7
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-08-03 06:40:24 +0000

    [v26.0.0] chore: Update ValkyrjaInfo for the release

--- src/valkyrja/constant/valkyrja_info.py
+++ src/valkyrja/constant/valkyrja_info.py
@@ -12,5 +12,5 @@ from typing import Final
 class ValkyrjaInfo:
     """Package version metadata, updated by the release workflow."""
 
-    VERSION: Final[str] = "26.0.2"
-    VERSION_BUILD_DATE_TIME: Final[str] = "August 2 2026 08:36:51 MST"
+    VERSION: Final[str] = "26.0.0"
+    VERSION_BUILD_DATE_TIME: Final[str] = "August 2 2026 23:39:21 MST"
